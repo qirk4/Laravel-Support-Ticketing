08880f8b5e1d1607e25f3adeff4976e785500a39
qirk4 <sztanyi.andras@gmail.com>
2023-03-09 11:15:25 +0100

    adding userdata.sh

---
+++ userdata.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install git
+sudo apt-get install php7.4-mbstring
+sudo apt-get install php7.4-xml
+sudo apt-get install php7.4-xmlwriter
+wget https://raw.githubusercontent.com/composer/getcomposer.org/76a7060ccb93902cd7576b67264ad91c8a2700e2/web/installer -O - -q | php -- --quiet
+composer update
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('sha384', 'composer-setup.php') === '55ce33d7678c5a611085589f1f3ddf8b3c52d662cd01d4ba75c0ee0459970c2200a51f492d557530c71c15d8dba01eae') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+composer update
+sudo mv composer.phar /usr/local/bin/composer
+cd /home
+git pull https://github.com/qirk4/Laravel-Support-Ticketing.git

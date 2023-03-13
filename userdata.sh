ffe1a14509fe4c1de091585c37cbb99646f4bdcc
qirk4 <sztanyi.andras@gmail.com>
2023-03-13 13:20:08 +0100

    Fixed userdata + installed make

--- userdata.sh
+++ userdata.sh
@@ -16,21 +16,8 @@ usermod -aG docker ubuntu
 # Install docker-compose
 curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
-sudo apt install software-properties-common
-echo yes | sudo add-apt-repository ppa:ondrej/php
-sudo apt-get install php7.4 -y
-sudo apt-get install php7.4-mbstring -y 
-sudo apt-get install php7.4-xml -y
-sudo apt-get install php7.4-xmlwriter -y
-wget https://raw.githubusercontent.com/composer/getcomposer.org/76a7060ccb93902cd7576b67264ad91c8a2700e2/web/installer -O - -q | php -- --quiet
-sudo apt install composer -y
-echo yes | composer update
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php -r "if (hash_file('sha384', 'composer-setup.php') === '55ce33d7678c5a611085589f1f3ddf8b3c52d662cd01d4ba75c0ee0459970c2200a51f492d557530c71c15d8dba01eae') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-php composer-setup.php
-php -r "unlink('composer-setup.php');"
-echo yes | composer update
-sudo mv composer.phar /usr/local/bin/composer
+apt install make
 cd /home/ubuntu
 git clone https://github.com/qirk4/Laravel-Support-Ticketing.git
+cd /home/ubuntu/Laravel-Support-Ticketing
 docker-compose up -d
